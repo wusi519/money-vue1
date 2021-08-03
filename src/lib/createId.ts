9beb25eb8780f6f91ae429aa4baa2e750e6205c0
wusi519 <327742525@qq.com>
2021-08-04 07:29:02 +0800

    开始写标签页

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,8 +1,8 @@
-let id: number = parseInt(window.localStorage.getItem('_maxId') || '0') || 0;
+let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
 
 function createId() {
   id += 1;
-  window.localStorage.setItem('_maxId', id.toString());
+  window.localStorage.setItem('_idMax', id.toString());
   return id;
 }
 
