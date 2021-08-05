0a413e3472719b83f7c508c9e4ca2e8378ee0c26
wusi519 <327742525@qq.com>
2021-08-06 05:45:34 +0800

    全部使用Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
 				<span class="rightSpan"></span>
 			</div>
 			<div class="form-wrapper">
-				<FormItem :value="tag.name" @update:value="update" field-name="标签名" place-holder="请输入标签名"/>
+				<FormItem :value="currentTag.name" @update:value="update" field-name="标签名" place-holder="请输入标签名"/>
 			</div>
 			<div class="button-wrapper">
 				<Button @click="remove">删除标签</Button>
@@ -26,7 +26,7 @@
     components: {Button, FormItem},
   })
   export default class EditLabels extends Vue {
-    get tag() {
+    get currentTag() {
       return this.$store.state.currentTag;
     }
 
@@ -34,20 +34,20 @@
       const id = this.$route.params.id;
       this.$store.commit('fetchTags');
       this.$store.commit('setCurrentTag', id);
-      if (!this.tag) {
+      if (!this.currentTag) {
         this.$router.replace('/404');
       }
     }
 
     update(name: string): void {
-      if (this.tag) {
-        this.$store.commit('updateTag', {id: this.tag.id, name});
+      if (this.currentTag) {
+        this.$store.commit('updateTag', {id: this.currentTag.id, name});
       }
     }
 
     remove() {
-      if (this.tag) {
-        this.$store.commit('removeTag', this.tag.id);
+      if (this.currentTag) {
+        this.$store.commit('removeTag', this.currentTag.id);
       }
     }
 
